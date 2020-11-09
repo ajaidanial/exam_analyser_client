b8c2a679f41a8724350225b16dafa07e30989948
ajaidanial <ajai@geopits.com>
2020-11-10 02:21:03 +0530

    home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import { Card } from 'react-bootstrap'
+import { triggerSimpleAjax } from '../helpers/httpHelper'
 
 export default class Home extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    triggerSimpleAjax('examination/dashboard-insights/', 'get').then(
+      (response) => {
+        this.setState({
+          ...this.state,
+          data: response
+        })
+      }
+    )
+  }
   render() {
     return (
       <div className="page-container">
-        <h1>home</h1>
+        <div className="m-4">
+          <div className="row">
+            {this.state.data.map((data, index) => (
+              <div className="col-md-3 p-2">
+                <Card className="p-3 shadow">
+                  <Card.Title className="text-primary">
+                    {data.display_name}
+                  </Card.Title>
+                  <Card.Title>{data.value}</Card.Title>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     )
   }
