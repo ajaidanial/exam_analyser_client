054149291c9998fbbc44413b05cd1e6c3f51d47f
ajaidanial <ajai@geopits.com>
2020-11-10 00:55:21 +0530

    nav fixed

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 export default class Navibar extends Component {
+  state = {
+    role: null
+  }
+
+  componentDidMount() {
+    let user_data = localStorage.getItem('user_data', null)
+    if (user_data) {
+      this.setState({
+        ...JSON.parse(user_data)
+      })
+    }
+  }
+
   render() {
+    let { role } = this.state
     return (
       <Navbar bg="light bg-dblue" expand="lg">
         <Navbar.Brand href="/home">SRCG</Navbar.Brand>
@@ -9,11 +23,23 @@ export default class Navibar extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/subjects">Subject</Nav.Link>
-            <Nav.Link href="/exams">Exams</Nav.Link>
-            <Nav.Link href="/members">Users</Nav.Link>
-            <Nav.Link href="/categories">Categories</Nav.Link>
-            <Nav.Link href="/examination/overview">Examination</Nav.Link>
+
+            {role === 'admin' && (
+              <>
+                <Nav.Link href="/members">Teachers</Nav.Link>
+              </>
+            )}
+
+            {role === 'teacher' && (
+              <>
+                <Nav.Link href="/subjects">Subjects</Nav.Link>
+                <Nav.Link href="/categories">Categories</Nav.Link>
+                <Nav.Link href="/exams">Exams</Nav.Link>
+                <Nav.Link href="/members">Students</Nav.Link>
+                <Nav.Link href="/examination/overview">Examination</Nav.Link>
+              </>
+            )}
+
             <Nav.Link
               onClick={(e) => {
                 localStorage.removeItem('auth_key')
